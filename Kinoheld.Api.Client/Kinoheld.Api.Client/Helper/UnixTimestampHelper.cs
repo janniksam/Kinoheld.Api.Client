e751509a0a7edb4e7f80748c24fe8b75d3223a0e
Jannik Kowalzik <hugo934753@web.de>
2018-10-09 22:38:54 +0200

    german time always, no matter what timezone the consumer has

--- Kinoheld.Api.Client/Kinoheld.Api.Client/Helper/UnixTimestampHelper.cs
+++ Kinoheld.Api.Client/Kinoheld.Api.Client/Helper/UnixTimestampHelper.cs
@@ -4,11 +4,12 @@ namespace Kinoheld.Api.Client.Helper
 {
     public static class UnixTimestampHelper
     {
-        public static DateTime ToDateTime(this long unixTimeStamp)
+        public static DateTime ToGermanDateTime(this long unixTimeStamp)
         {
             var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
-            return dtDateTime;
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp);
+            return TimeZoneInfo.ConvertTime(
+                dtDateTime, TimeZoneInfo.Utc, TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time"));
         }
     }
 }
