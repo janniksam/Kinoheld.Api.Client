1836060f0b1339573faf0658a4d484a4cc249a56
Jannik Kowalzik <hugo934753@web.de>
2018-10-09 23:33:05 +0200

    german time fix

--- Kinoheld.Api.Client/Kinoheld.Api.Client/Helper/UnixTimestampHelper.cs
+++ Kinoheld.Api.Client/Kinoheld.Api.Client/Helper/UnixTimestampHelper.cs
@@ -6,10 +6,9 @@ namespace Kinoheld.Api.Client.Helper
     {
         public static DateTime ToGermanDateTime(this long unixTimeStamp)
         {
-            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp);
-            return TimeZoneInfo.ConvertTime(
-                dtDateTime, TimeZoneInfo.Utc, TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time"));
+            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).AddHours(2);
+            return dtDateTime;
         }
     }
 }
