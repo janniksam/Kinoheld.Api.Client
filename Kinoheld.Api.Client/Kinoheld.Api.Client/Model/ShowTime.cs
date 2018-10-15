f050c29139b98b014f5b8ace30941524f12c38ce
Jannik <Jannik@Github>
2018-10-15 13:42:55 +0200

    Problems with DST - possible fix

--- Kinoheld.Api.Client/Kinoheld.Api.Client/Model/ShowTime.cs
+++ Kinoheld.Api.Client/Kinoheld.Api.Client/Model/ShowTime.cs
@@ -11,7 +11,7 @@ namespace Kinoheld.Api.Client.Model
 
         public DateTime GetDateTime()
         {
-            return Timestamp.ToGermanDateTime();
+            return Timestamp.ToDateTime().ToLocalTime();
         }
     }
 }
